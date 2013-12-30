10f5a21b3a479adc814204f2169b3b324c9a175f
toooooooby <toby.net.info.mail+git@gmail.com>
2013-12-30 21:23:45 +0900

    Fix for newer rspec version

--- spec/chek_spec.rb
+++ spec/chek_spec.rb
@@ -6,22 +6,22 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__)))
 describe Chek do
   it "normal require" do
     expect { require 'foobar-can-not-require' }.to raise_error(LoadError)
-    expect { require 'foobar-normal' }.to_not raise_error(LoadError)
+    expect { require 'foobar-normal' }.not_to raise_error(LoadError)
     FoobarNormal.piyo.should == "piyopiyo"
   end
 
   context '☑' do
     it "require" do
       expect { ☑ 'foobar-can-not-require' }.to     raise_error(LoadError)
-      expect { ☑ 'foobar-with-chek'       }.to_not raise_error(LoadError)
+      expect { ☑ 'foobar-with-chek'       }.not_to raise_error(LoadError)
       FoobarWithChek.piyo.should == "piyopiyo"
     end
   end
 
   context '☐' do
     it "does not require" do
-      expect { ☐ 'foobar-can-not-require' }.to_not raise_error(LoadError)
-      expect { ☐ 'foobar-without-chek'    }.to_not raise_error(LoadError)
+      expect { ☐ 'foobar-can-not-require' }.not_to raise_error(LoadError)
+      expect { ☐ 'foobar-without-chek'    }.not_to raise_error(LoadError)
       expect { FoobarWithoutChek.piyo     }.to raise_error(NameError)
     end
   end
