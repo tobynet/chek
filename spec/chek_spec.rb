cf0d0169326ef42c6a314e0255f20821fdabd092
tobynet <toby.net.info.mail+git@gmail.com>
2014-11-01 20:26:54 +0900

    transpec gem

--- spec/chek_spec.rb
+++ spec/chek_spec.rb
@@ -6,22 +6,22 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__)))
 describe Chek do
   it "normal require" do
     expect { require 'foobar-can-not-require' }.to raise_error(LoadError)
-    expect { require 'foobar-normal' }.not_to raise_error(LoadError)
-    FoobarNormal.piyo.should == "piyopiyo"
+    expect { require 'foobar-normal' }.not_to raise_error
+    expect(FoobarNormal.piyo).to eq("piyopiyo")
   end
 
   context '☑' do
     it "require" do
       expect { ☑ 'foobar-can-not-require' }.to     raise_error(LoadError)
-      expect { ☑ 'foobar-with-chek'       }.not_to raise_error(LoadError)
-      FoobarWithChek.piyo.should == "piyopiyo"
+      expect { ☑ 'foobar-with-chek'       }.not_to raise_error
+      expect(FoobarWithChek.piyo).to eq("piyopiyo")
     end
   end
 
   context '☐' do
     it "does not require" do
-      expect { ☐ 'foobar-can-not-require' }.not_to raise_error(LoadError)
-      expect { ☐ 'foobar-without-chek'    }.not_to raise_error(LoadError)
+      expect { ☐ 'foobar-can-not-require' }.not_to raise_error
+      expect { ☐ 'foobar-without-chek'    }.not_to raise_error
       expect { FoobarWithoutChek.piyo     }.to raise_error(NameError)
     end
   end
